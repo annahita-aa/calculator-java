608b849846666c51205ba9532dbfeaaeda381977
Annahita <aalipourannahita@gmail.com>
2024-06-18 18:11:36 +0330

    fourth commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,22 @@
 package org.example;
 
+import java.util.List;
 import java.util.Scanner;
 
 /**
  * Calculator Without operator priority
+ * and
+ * Calculator With operator priority
  */
 public class App {
     public static void main(String[] args) {
 
         Calculator calc1 = new Calculator();
-        String expression= calc1.enterYourExpression();
-        calc1.calculateExpression(expression);
+        List<String> tokens= calc1.enterYourExpression();
+        System.out.println("this is your answer without priorities: ");
+        calc1.calculateExpressionWithoutPriorities(tokens);
+        System.out.println("this is your answer with priorities: ");
+        calc1.calculateExpressionWithPriorities(tokens);
 
 
     }
--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,52 +1,77 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Calculator {
 
     private String expression;
+    private double operand = 0;
+    private String operator = "+";
+    private List<String> tokens;
+
+    private String repalceNumber;
 
 
-    public String enterYourExpression() {
+    public List<String> enterYourExpression() {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter expression: ");
-        return expression = scanner.nextLine();
-
+        expression = scanner.nextLine();
+        // Split by valid numbers and special characters
+        return tokens = new ArrayList<>(Arrays.asList(expression.split("(?<=\\d)(?=[^\\d.])|(?<=[^\\d.])(?=\\d)")));
     }
 
-    public String calculateExpression(String expression){
-
-
-
-        String[] tokens = expression.split("(?<=\\d)(?=[^\\d.])|(?<=[^\\d.])(?=\\d)"); // Split by valid numbers and special characters
-
-
-
-        double operand = 0;
-        String operator = "+";
+    public void calculateExpressionWithoutPriorities(List<String> tokens) {
 
         for (String token : tokens) {
             if (token.matches("[0-9.]+")) {
                 operand = recalculateOperand(token, operand, operator);
-            } else if (token.matches("^[+*/-]$"))  {
+            } else if (token.matches("^[+*/-]$")) {
                 operator = token;
             } else {
-                System.err.println("invalid operator: " +token);
-                System.exit(0);
+                System.err.println("invalid operator: " + token);
             }
 
         }
         System.out.println(operand);
-        return null;
+
     }
+    public void calculateExpressionWithPriorities(List<String> tokens) {
+
+        for (int i = 0; i < tokens.toArray().length; i++) {
+            if (tokens.get(i).matches("^[*/]$")) {
+                if (tokens.get(i).equals("*")) {
+                    repalceNumber = Double.toString(Double.parseDouble(tokens.get(i - 1)) * Double.parseDouble(tokens.get(i + 1)));
+                } else {
+                    repalceNumber = Double.toString(Double.parseDouble(tokens.get(i - 1)) / Double.parseDouble(tokens.get(i + 1)));
+                }
+                tokens.set(i - 1, repalceNumber);
+                tokens.remove(i);
+                tokens.remove(i);
+                i--;
+                continue;
+            }
+            if (tokens.get(i).matches("^[+-]$")) {
+                continue;
+            }
+
+        }
+        operand = 0;
+        operator ="+";
+        calculateExpressionWithoutPriorities(tokens);
+
+    }
+
+
 
     private static double recalculateOperand(String token, Double operand, String operator) {
 
 
-
         try {
-           double number = Double.parseDouble(token);
+            double number = Double.parseDouble(token);
             if (operator.equals("+")) {
                 return number = operand + number;
             }
@@ -60,7 +85,7 @@ public class Calculator {
                 return number = operand / number;
             }
         } catch (NumberFormatException e) {
-            System.err.println("this is not a valid number: "+ token);
+            System.err.println("this is not a valid number: " + token);
             System.err.println("this is your final calculated before input invalid number: ");
         }
         return operand;
