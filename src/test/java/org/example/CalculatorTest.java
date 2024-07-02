8dc31f5e8b6ce36d219abc2c132c2348f37d48d6
Annahita Aalipour <aalipourannhita@gmail.com>
2024-07-02 18:57:32 +0330

    unit testing
    add some test file to project
    
    working on #1

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -2,11 +2,16 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 import java.util.List;
-
-import static junit.framework.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
+class CalculatorTest {
+
+    @Test
+    void testEnterYourExpression() {
+        Calculator calc = new Calculator();
+        List<String> tokens = calc.enterYourExpression("3+5*2");
+        assertEquals(List.of("3", "+", "5", "*", "2"), tokens);
+    }
 
     @Test
     void testCalculateExpressionWithoutPriorities() {
@@ -22,5 +27,13 @@ class AppTest {
         List<String> tokens = List.of("3", "+", "5", "*", "2");
         calc.calculateExpressionWithPriorities(tokens);
         assertEquals(13.0, calc.getOperand());
+    }
+
+    @Test
+    void testRecalculateOperand() {
+        assertEquals(8.0, Calculator.recalculateOperand("5", 3.0, "+"));
+        assertEquals(15.0, Calculator.recalculateOperand("5", 3.0, "*"));
+        assertEquals(-2.0, Calculator.recalculateOperand("5", 3.0, "-"));
+        assertEquals(0.6, Calculator.recalculateOperand("5", 3.0, "/"));
     }
 }
